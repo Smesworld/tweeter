80f1719cd64879758a1a5989234fac6c0bcd311d
Alan Dodge <alphaechododge@gmail.com>
2019-10-08 20:46:36 +0000

    Finished counter changing color below 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   $("[name='text']").keyup(function() {
-    const num = $(this).val().length; //The this keyword is a reference to the button
-    console.log(num);
-    const thing = $(this).siblings("span");
-    console.log(thing);
-    const newNum = 140 - num;
-    $(thing[0]).text(newNum);
+    const inputLength = $(this).val().length; // Get number of chars in input
+    const $span = $(this).siblings("span")[0]; // Get span sibling
+    const charsLeft = 140 - inputLength;
+    if (charsLeft < 0) {
+      $($span).text(charsLeft).addClass("negative");
+    } else {
+      $($span).text(charsLeft).removeClass("negative");
+    }
   });
 });
